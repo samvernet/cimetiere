777f265d8e286ea263ad60e795c4047f40dbfe35
samvernet <148976408+samvernet@users.noreply.github.com>
2026-02-11 14:26:39 +0100

    index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,14 @@
-
+if (typeof document !== 'undefined') {
+  const style = document.createElement('style');
+  style.innerHTML = `
+    body {
+      padding-bottom: env(safe-area-inset-bottom) !important;
+      min-height: 100vh;
+      box-sizing: border-box;
+    }
+  `;
+  document.head.appendChild(style);
+}
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
